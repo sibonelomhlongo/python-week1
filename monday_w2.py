2dcc389d7217164ae9e6a2a2c9e87f677168b4c4
sibonelomhlongo <162976791+sibonelomhlongo@users.noreply.github.com>
2024-03-18 11:53:44 +0200

    b

---
+++ monday_w2.py
@@ -0,0 +1,29 @@
+# dictionaries
+#method_1
+mydict21 = {"name1" : "sibonelo", "age1" : 25, "city1" : "capetown"}
+print(mydict21)
+#method_2
+mydict2 = dict(name="sibonelo", age=27, city="capetown")
+print(mydict2)
+
+'''value = mydict21("name")
+print(value)'''
+#add key with value on dictionary
+
+mydict2["surname"] = "mhlongo"
+print(mydict2)
+
+#delete
+mydict2.pop("age")
+print(mydict2)
+
+#exceptional statement
+
+try:
+    print(mydict21["city"])
+except:
+    print('KeyError')
+
+for key, value in mydict21.items:
+    print(key, value)
+
