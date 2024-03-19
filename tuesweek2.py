ff09f4e23107fc427d992f2e26e27fee6db0a434
sibonelomhlongo <162976791+sibonelomhlongo@users.noreply.github.com>
2024-03-19 09:28:24 +0200

    gng

---
+++ tuesweek2.py
@@ -0,0 +1,17 @@
+#converting string to list
+mystring = "how are you doing today"
+mylist = list(mystring) # each letter as item in list including space 
+print(mylist)
+mylist1 = mystring.split() # each word become item in list
+print(mylist1)
+mylist2 = ''.join(mylist1) 
+print(mylist2)
+
+var = "SIBONELO"
+sentence = "how are you doing today %s" % var
+print(sentence)
+sentence2 = "sibonelo %s" % mystring
+print(sentence2)
+var2 = 4.9777732
+sentence3 = "sibonelo {} {}".format(mystring,var2)
+print(sentence3)
