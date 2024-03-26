86f92f990c6c621c81caa8ca6380ba6e153957da
sibonelomhlongo <162976791+sibonelomhlongo@users.noreply.github.com>
2024-03-26 12:16:22 +0200

    h

--- tuesdayweek3.py
+++ tuesdayweek3.py
@@ -10,3 +10,15 @@ def printname():
     print("alex")
 printname = startend_decorator(printname) 
 printname()
+
+class employee:
+    def __init__(self,first,last,pay) -> None:
+        self.first = first
+        self.last = last
+        self.pay =pay
+        self.email = first+"."+last+"@company.com"
+    pass
+pass
+emp1= employee("sibo","mhlongo",12000)
+print(emp1.email)
+print("{} {}".format(emp1.first,emp1.last))
