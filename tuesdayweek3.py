8b89bc7de16896602435bd8b4778a52bf6364047
sibonelomhlongo <162976791+sibonelomhlongo@users.noreply.github.com>
2024-03-26 15:33:49 +0200

    cbv

--- tuesdayweek3.py
+++ tuesdayweek3.py
@@ -13,12 +13,11 @@ printname()
 
 class employee:
     def __init__(x,first,last,pay) -> None:
-        x.first = 'sibonelo'
+        x.first = first
         x.last = last
         x.pay =pay
         x.email = first+"."+last+"@company.com"
-    def getFirst(x):
-       return x.first
+     
 
 
 emp1= employee("sibo","mhlongo",12000)
@@ -26,4 +25,9 @@ emp2=employee('tshidi','pholo',100000)
 print(emp1.email)
 print("{} {}".format(emp2.first,emp2.last))
 print("{} {}".format(emp1.first,emp1.last))
-print(emp2.getFirst())
+#print(emp2)
+class worker(employee):
+    def __init__(x, first, last, pay) -> None:
+        super().__init__(first, last, pay)
+emp9= worker("sibo","mhlongo",12000)
+print("{} {}".format(emp9.first,emp9.last))
