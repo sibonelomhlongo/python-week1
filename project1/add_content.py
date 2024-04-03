4ff65e221be346d6325d0d6e2e018cc184517ff3
sibonelomhlongo <162976791+sibonelomhlongo@users.noreply.github.com>
2024-04-03 11:33:48 +0200

    fasd

---
+++ project1/add_content.py
@@ -0,0 +1,10 @@
+def get_random_qouets():
+    pass
+def get_forecast():
+    pass
+def get_trends():
+    pass
+def get_trends():
+    pass
+if __name__=="__main__":
+    pass
---
+++ project1/creat_gui.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+class dailioigestGUI:
+
+    def __init__(self, root) -> None:
+        pass
+if __name__=='__main':
+    root = Tk
+    app= dailioigestGUI(root)
+    root.mainloop()
